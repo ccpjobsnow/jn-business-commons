7151575b31363fc2eb5959aae6e2afba6732ffd9
onias <onias@noxxonsat.com.br>
2022-11-15 16:22:43 -0300

    RemoveResume, GetRecruiterDomains, GetCandidate, DownloadResumeText, DownloadResumeFile

---
+++ src/main/java/com/jn/commons/JnCacheKeys.java
@@ -0,0 +1,8 @@
+package com.jn.commons;
+
+public interface JnCacheKeys {
+
+	String RESUMES_KEY = "RESUMES";
+	String RECRUITERS_DOMAINS_KEY = "RECRUITERS_DOMAINS";
+	
+}
---
+++ src/main/java/com/jn/commons/JnConstants.java
@@ -0,0 +1,10 @@
+package com.jn.commons;
+
+public interface JnConstants {
+	String RESUMES_BUCKET = "JN_RESUMES";
+	String TENANT = "JN_TENANT";
+	String SLASH_FILE = "/file";
+	String DOT = ".";
+	int ONE_HOUR_IN_SECONDS = 60 * 60;
+	String SLASH_TEXT = "/text";
+}
