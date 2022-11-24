ba6bdc54ac3eb321bdc49f67bc5ed8f4c2814c4f
onias <onias@noxxonsat.com.br>
2022-11-23 23:44:49 -0300

    GetResumesList, GetResumesStatis

--- src/main/java/com/jn/commons/tables/fields/A3D_candidate.java
+++ src/main/java/com/jn/commons/tables/fields/A3D_candidate.java
@@ -3,6 +3,6 @@ package com.jn.commons.tables.fields;
 import com.ccp.especifications.db.table.CcpDbTableField;
 
 public enum A3D_candidate implements CcpDbTableField{
-		audit, email, resume, keywords, currentJob, resumeWords, synonyms, desiredJob, pcd, experience, seniority, ddd
+		audit, email, resume, keywords, currentJob, resumeWords, synonyms, desiredJob, pcd, experience, seniority, ddd, pj, clt, btc
 	;
 }
