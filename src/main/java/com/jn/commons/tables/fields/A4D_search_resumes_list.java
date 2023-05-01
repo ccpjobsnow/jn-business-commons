5ac01dd9374205ff02a48c3f414a743e00692397
onias <onias@noxxonsat.com.br>
2023-05-01 19:05:42 -0300

    solução dos TODOS

--- src/main/java/com/jn/commons/tables/fields/A4D_search_resumes_list.java
+++ src/main/java/com/jn/commons/tables/fields/A4D_search_resumes_list.java
@@ -2,7 +2,6 @@ package com.jn.commons.tables.fields;
 
 import com.ccp.especifications.db.utils.CcpDbTableField;
 
-//TODO A FORMAÇÃO DE KEY TEM QUE CONSIDERAR LIST
 public enum A4D_search_resumes_list implements CcpDbTableField{
 	audit(false),
 	requiredKeywords(true),
