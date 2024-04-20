2114f265542bb2287064e8b6923a8e423a22f598
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-04-19 23:56:25 -0300

    .

--- src/main/java/com/jn/commons/utils/JnTopics.java
+++ src/main/java/com/jn/commons/utils/JnTopics.java
@@ -1,36 +1,5 @@
 package com.jn.commons.utils;
 
-import com.ccp.especifications.mensageria.sender.CcpTopic;
-
-public enum JnTopics implements CcpTopic {
-	requestTokenAgain{
-
-		public String getTopicName() {
-			return null;
-		}
-		
-	},
-	requestUnlockToken{
-
-		public String getTopicName() {
-			return null;
-		}
-	}, sendUserToken {
-		public String getTopicName() {
-			return null;
-		}
-	}, removeTries {
-		public String getTopicName() {
-			return null;
-		}
-	}, notifyError {
-		public String getTopicName() {
-			return null;
-		}
-	}, notifyContactUs {
-		public String getTopicName() {
-			return null;
-		}
-	},
-	;
+public enum JnTopics {
+	requestTokenAgain, requestUnlockToken, sendUserToken, removeTries, notifyError, notifyContactUs,;
 }
