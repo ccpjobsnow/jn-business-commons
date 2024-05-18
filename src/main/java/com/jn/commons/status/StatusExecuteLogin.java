a9e8de22e5e4b87cf95ceef34df50a92641c50db
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-05-18 02:22:21 -0300

    Separação dos filters de colocar dados da sessão (IP, token, useragent (navegador)), validação de sessão, validação de e-mail... modularização (por meio de filter) da lógica que preenche dados da sessão para todos os jsons de todos os endpoints

---
+++ src/main/java/com/jn/commons/status/StatusExecuteLogin.java
@@ -0,0 +1,29 @@
+package com.jn.commons.status;
+
+import com.ccp.process.CcpProcessStatus;
+
+public enum StatusExecuteLogin implements CcpProcessStatus{
+	invalidEmail(400),
+	lockedToken(403),
+	missingEmail(404),
+	lockedPassword(423),
+	wrongPassword(421),
+	passwordLockedRecently(429),
+	loginConflict(409),
+	missingPassword(202),
+	expectedStatus(200),
+	invalidSession(401),
+	;
+
+	public final int status;
+	
+	
+	
+	private StatusExecuteLogin(int status) {
+		this.status = status;
+	}
+
+	public int status() {
+		return status;
+	}
+}
---
+++ src/main/java/com/jn/commons/utils/JnValidateSession.java
@@ -0,0 +1,31 @@
+package com.jn.commons.utils;
+
+import java.util.function.Function;
+
+import com.ccp.decorators.CcpJsonRepresentation;
+import com.ccp.especifications.db.crud.CcpGetEntityId;
+import com.jn.commons.entities.JnEntityLoginEmail;
+import com.jn.commons.entities.JnEntityLoginPassword;
+import com.jn.commons.entities.JnEntityLoginSessionToken;
+import com.jn.commons.entities.JnEntityLoginToken;
+import com.jn.commons.status.StatusExecuteLogin;
+
+public class JnValidateSession implements Function<CcpJsonRepresentation, CcpJsonRepresentation>{
+
+	private JnValidateSession() {}
+	
+	public static final JnValidateSession INSTANCE = new JnValidateSession();
+	
+	public CcpJsonRepresentation apply(CcpJsonRepresentation values) {
+		new CcpGetEntityId(values)
+		.toBeginProcedureAnd()
+			.ifThisIdIsPresentInEntity(JnEntityLoginToken.INSTANCE.getMirrorEntity()).returnStatus(StatusExecuteLogin.lockedToken).and()
+			.ifThisIdIsPresentInEntity(JnEntityLoginPassword.INSTANCE.getMirrorEntity()).returnStatus(StatusExecuteLogin.lockedPassword).and()
+			.ifThisIdIsNotPresentInEntity(JnEntityLoginPassword.INSTANCE).returnStatus(StatusExecuteLogin.missingPassword).and()
+			.ifThisIdIsNotPresentInEntity(JnEntityLoginEmail.INSTANCE).returnStatus(StatusExecuteLogin.missingEmail).and()
+			.ifThisIdIsNotPresentInEntity(JnEntityLoginSessionToken.INSTANCE).returnStatus(StatusExecuteLogin.invalidSession).andFinallyReturningThisFields("sessionToken")
+		.endThisProcedureRetrievingTheResultingData();
+		return values;
+	}
+
+}
