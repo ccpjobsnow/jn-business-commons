751635ecaa29f363d3a26d18e407557c19aeffeb
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-07-07 02:14:48 -0300

    .

--- src/main/java/com/jn/commons/utils/JnTopic.java
+++ src/main/java/com/jn/commons/utils/JnTopic.java
@@ -1,6 +1,40 @@
 package com.jn.commons.utils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.ccp.validation.annotations.SimpleObject;
+import com.ccp.validation.annotations.ValidationRules;
+import com.ccp.validation.enums.SimpleObjectValidations;
+
 public interface JnTopic {
 	String name();
 	Class<?> validationClass();
+	
+	default String[] getFields() {
+		Class<?> clazz = this.validationClass();
+		ValidationRules rules = clazz.getAnnotation(ValidationRules.class);
+		ArrayList<String> list = new ArrayList<>();
+		SimpleObject[] simpleObject = rules.simpleObject();
+		for (SimpleObject so : simpleObject) {
+			SimpleObjectValidations rule = so.rule();
+			boolean requiredFields = SimpleObjectValidations.requiredFields.equals(rule);
+			if(requiredFields) {
+				String[] fields = so.fields();
+				List<String> asList = Arrays.asList(fields);
+				list.addAll(asList);
+			}
+			boolean requiredAtLeastOne = SimpleObjectValidations.requiredAtLeastOne.equals(rule);
+			if(requiredAtLeastOne) {
+				String[] fields = so.fields();
+				List<String> asList = Arrays.asList(fields);
+				list.addAll(asList);
+			}
+		}
+		
+		String[] array = list.toArray(new String[list.size()]);
+		return array;
+		
+	}
 }
